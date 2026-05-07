685a61bca839d0d9c5d0b04ac38597a2d76fe3a6
Kayyleb <kaylebjuliancook@gmail.com>
2026-05-07 15:10:37 -0700

    Folder Name change

--- CST250_AssemblyProgramming/LAB_UNO.X/build/default/production/Lab1.o.d
+++
@@ -1,8 +0,0 @@
-build/default/production/Lab1.o:  \
-Lab1.S 
-build/default/production/Lab1.o:  \
- /Applications/microchip/mplabx/v6.00/packs/Microchip/PIC32MX_DFP/1.5.259/include/proc/p32mx360f512l.h  \
- /Applications/microchip/mplabx/v6.00/packs/Microchip/PIC32MX_DFP/1.5.259/include/xc-pic32m.h  \
- /Applications/microchip/mplabx/v6.00/packs/Microchip/PIC32MX_DFP/1.5.259/include/xc.h  \
-  \
-Lab1.S  \
